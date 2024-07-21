d089f76b6699b340598b172074c306633ac354b3
Viete <phamlequocviet1207@gmail.com>
2024-07-21 13:02:24 -0400

    fixing bugs (break when no face show) + change code

--- face_detect_camera.py
+++ face_detect_camera.py
@@ -12,18 +12,19 @@ with face_detect.FaceDetection(min_detection_confidence=0.5, model_selection=0) 
         out = face_detect.process(frame_rgb)
         H, W, _ = frame.shape
 
-        for detection in out.detections:
-            location_data = detection.location_data
-            bbox = location_data.relative_bounding_box
+        if out.detections:
+            for detection in out.detections:
+                location_data = detection.location_data
+                bbox = location_data.relative_bounding_box
 
-            x1, y1, w, h, = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+                x1, y1, w, h, = bbox.xmin, bbox.ymin, bbox.width, bbox.height
 
-            x1 = int(x1 * W)
-            y1 = int(y1 * H)
-            w = int(w * W)
-            h = int(h * H)
+                x1 = int(x1 * W)
+                y1 = int(y1 * H)
+                w = int(w * W)
+                h = int(h * H)
 
-            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
+                cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
 
 
 
