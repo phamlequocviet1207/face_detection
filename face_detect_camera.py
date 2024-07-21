475dae172ecef1ba7209822e9f10eccc8533bc88
Viete <phamlequocviet1207@gmail.com>
2024-07-21 12:49:14 -0400

    base on code in face_detect_img to detect face in realtime using camera

---
+++ face_detect_camera.py
@@ -0,0 +1,36 @@
+import cv2
+import mediapipe as mp
+
+face_detect = mp.solutions.face_detection
+
+cap = cv2.VideoCapture(0)
+
+with face_detect.FaceDetection(min_detection_confidence=0.5, model_selection=0) as face_detect:
+    while True:
+        _, frame = cap.read()
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        out = face_detect.process(frame_rgb)
+        H, W, _ = frame.shape
+
+        for detection in out.detections:
+            location_data = detection.location_data
+            bbox = location_data.relative_bounding_box
+
+            x1, y1, w, h, = bbox.xmin, bbox.ymin, bbox.width, bbox.height
+
+            x1 = int(x1 * W)
+            y1 = int(y1 * H)
+            w = int(w * W)
+            h = int(h * H)
+
+            cv2.rectangle(frame, (x1, y1), (x1 + w, y1 + h), (0, 255, 0), 10)
+
+
+
+        cv2.imshow('frame',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
